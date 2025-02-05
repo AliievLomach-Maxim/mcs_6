f5364c560274c2fadcaf90803a0eaf4709d619b3
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2025-02-05 21:09:57 +0200

    +

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,106 @@
-import { useState } from 'react'
+// import { useEffect, useState } from 'react'
+
+// // StrictMode > App (mounting) > App (unmounting) > App DOM (mounting)
+
+// const getDefaultValue = () => {
+//   const localData = localStorage.getItem('counter')
+//   if (localData) {
+//     return JSON.parse(localData)
+//   }
+//   return 123
+// }
+
+// const App = () => {
+//   const [counter, setCounter] = useState(getDefaultValue)
+
+//   const [counter2, setCounter2] = useState(100)
+
+//   // useEffect(() => {
+//   //   const localData = localStorage.getItem('counter')
+//   //   if (localData) {
+//   //     setCounter(JSON.parse(localData))
+//   //   }
+//   // }, [])
+
+//   useEffect(() => {
+//     // if (counter !== 0)
+//     localStorage.setItem('counter', counter)
+//   }, [counter])
+
+//   return (
+//     <div>
+//       <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+//       <br />
+//       <button onClick={() => setCounter2(counter2 + 1)}>{counter2}</button>
+//       <hr />
+//       <hr />
+//     </div>
+//   )
+// }
+
+// export default App
+// const App = () => {
+//   const [counter, setCounter] = useState(0)
+//   const [counter2, setCounter2] = useState(100)
+//   const [isShowTimer, setIsShowTimer] = useState(false)
+
+//   // mounting
+//   useEffect(() => {
+//     console.log('mounting')
+//   }, [])
+
+//   // mounting + update
+//   useEffect(() => {
+//     console.log('mounting + update', counter)
+//   }, [counter])
+
+//   // update
+//   useEffect(() => {
+//     if (counter !== 0) console.log('update counter', counter)
+//   }, [counter])
+
+//   // // update
+//   // useEffect(() => {
+//   //   console.log('mounting + update', counter)
+//   //   if (counter !== 0) {
+//   //     console.log('update counter', counter)
+//   //   }
+//   // }, [counter])
+
+//   // update
+//   useEffect(() => {
+//     if (counter2 !== 100) console.log('update counter2', counter2)
+//   }, [counter2])
+
+//   return (
+//     <div>
+//       <button onClick={() => setCounter(counter + 1)}>{counter}</button>
+//       <br />
+//       <button onClick={() => setCounter2(counter2 + 1)}>{counter2}</button>
+//       <hr />
+//       <hr />
+//       <hr />
+//       <button onClick={() => setIsShowTimer(!isShowTimer)}>toggle</button>
+//       {isShowTimer && <Timer />}
+//     </div>
+//   )
+// }
+
+// export default App
+import { useEffect, useState } from 'react'
 import posts from '../data.json'
 import Paginator from './components/Paginator/Paginator'
 import Post from './components/Post/Post'
 
-const App = () => {
-  const [postNumber, setPostNumber] = useState(1)
+const getDefaultPage = () => {
+  const localPage = localStorage.getItem('postNumber')
+  if (localPage) return JSON.parse(localPage)
+  return 1
+}
 
-  // const handleNext = () => {
-  //   if (postNumber === posts.length) setPostNumber(1)
-  //   else setPostNumber(postNumber + 1)
-  // }
+const App = () => {
+  const [postNumber, setPostNumber] = useState(getDefaultPage)
+
   const handleNext = () => {
     setPostNumber(postNumber + 1)
   }
@@ -22,6 +113,10 @@ const App = () => {
   const isLast = postNumber === posts.length
 
   const curPost = posts[postNumber - 1]
+
+  useEffect(() => {
+    localStorage.setItem('postNumber', postNumber)
+  }, [postNumber])
 
   return (
     <div>
@@ -40,74 +135,3 @@ const App = () => {
 }
 
 export default App
-// import { useState } from 'react'
-
-// const Clicker = ({ children, toChangeClicks, value }) => {
-//   return (
-//     <button onClick={toChangeClicks}>
-//       {children}: {value}
-//     </button>
-//   )
-// }
-// //
-// const App = () => {
-//   const [clicks, setClicks] = useState({
-//     value1: 0,
-//     value2: 10,
-//   })
-
-//   const handleClick = (key) => {
-//     setClicks({
-//       ...clicks,
-//       [key]: clicks[key] + 1,
-//     })
-//   }
-
-//   return (
-//     <div>
-//       <Clicker value={clicks.value1} toChangeClicks={() => handleClick('value1')}>
-//         Click 1
-//       </Clicker>
-//       <Clicker value={clicks.value2} toChangeClicks={() => handleClick('value2')}>
-//         Click 2
-//       </Clicker>
-//       <hr />
-//       <h1>Total:{clicks.value1 + clicks.value2}</h1>
-//     </div>
-//   )
-// }
-
-// export default App
-// Підняття стану
-// import { useState } from 'react'
-
-// const Clicker = ({ children, toChangeClicks, counter }) => {
-//   return (
-//     <button onClick={toChangeClicks}>
-//       {children}: {counter}
-//     </button>
-//   )
-// }
-// //
-// const App = () => {
-//   const [clicks, setClicks] = useState(0)
-
-//   const handleClick = () => {
-//     setClicks(clicks + 1)
-//   }
-//   return (
-//     <div>
-//       <Clicker counter={clicks} toChangeClicks={handleClick}>
-//         Click 1
-//       </Clicker>
-//       <Clicker counter={clicks} toChangeClicks={handleClick}>
-//         Click 2
-//       </Clicker>
-//       <h1>Total:{clicks}</h1>
-//       <hr />
-//       {/* <button onClick={handleClick}>clicks: {counter}</button> */}
-//     </div>
-//   )
-// }
-
-// export default App
