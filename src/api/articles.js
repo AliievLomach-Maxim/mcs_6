bdb3683a55d85fc6a732dcc3421e996668fd9d73
AliievLomachMaxim <aliev.lomach.maxim@gmail.com>
2025-02-17 21:14:50 +0200

    +

---
+++ src/api/articles.js
@@ -0,0 +1,13 @@
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://hn.algolia.com/api/v1'
+
+export const fetchArticles = async (query, page) => {
+  const { data } = await axios.get('/search', {
+    params: {
+      query,
+      page,
+    },
+  })
+  return data.hits
+}
